133e6d6917ef7613a1cdb68b004400f65e137907
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2022-07-11 22:34:31 +0300

    Create emptier.js

---
+++ emptier.js
@@ -0,0 +1,9 @@
+const selectedContainers = document.querySelectorAll('._OxEpxzAgJiTENfolVUN')
+
+const containers = Object.keys(selectedContainers).map((key) => {
+    return selectedContainers[key];
+})
+
+containers.map((item) => {
+    return item.querySelector('.TT1tIewS2iI8Uz8kLuQB').querySelector('.DbMYFmOEEz9PH1h1zK9n').querySelector('.Fm7C3gdh5Lsc9qSXrQwO').click();
+})
